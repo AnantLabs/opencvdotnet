66a49a5321fddeb9413ed30b842177d5e83603ba
elad.benisrael <elad.benisrael@05b48626-c427-0410-92d5-518f3ac39d56>
2007-02-11 20:55:11 +0000

    Added MaskedHistogramTracker to Examples project. Added a solution that shows all C# projects together (not part of installation).

--- examples/RegionGrowing.cs
+++ examples/RegionGrowing.cs
@@ -54,7 +54,7 @@ namespace OpenCVDotNet.Examples
                     using (CVImage rg = videoPlayer.Capture.CreateCompatibleImage())
                     {
                         rg.Zero();
-                        RegionGrowing(midPoint, rg, new RegionGrowingCriteria(MyRGCriteria), videoPlayer.LastFrame);
+                        OpenCVDotNet.Algorithms.RegionGrowing.Fill(midPoint, rg, new OpenCVDotNet.Algorithms.RegionGrowingCriteria(MyRGCriteria), videoPlayer.LastFrame);
                         growingFrame.Image = rg.ToBitmap();
 
                         if (overlay.Checked)
@@ -129,47 +129,6 @@ namespace OpenCVDotNet.Examples
             bgFrame = args.NewCapture.CreateCompatibleImage();
         }
 
-        private CVPair[] connections = 
-        { 
-            new CVPair(-1, -1),
-            new CVPair(-1, 0),
-            new CVPair(-1, 1),
-            new CVPair(0, -1),
-            new CVPair(0, 1),
-            new CVPair(1, -1),
-            new CVPair(1, 0),
-            new CVPair(1, 1) 
-        };
-
-        /// <summary>
-        /// Implements an 8-connected region growing algorithm based on histogram similarity (using back projection).
-        /// Starts with the starting pixel and checks in the back projection all the four pixel around it.
-        /// If the pixels pass some threshold, they are selected and the region is grown to contain them as well.
-        /// </summary>
-        private void RegionGrowing(Point startPoint, CVImage outputImage, RegionGrowingCriteria criteria, object criteriaCookie)
-        {
-            // first, include the starting pixel in the region.
-            outputImage[startPoint] = new CVRgbPixel(255, 0, 0);
-
-            // go over all the connection points.
-            foreach (CVPair connection in connections)
-            {
-                Point connectionPoint = new Point(
-                        startPoint.X + (int) connection.Second, 
-                        startPoint.Y + (int) connection.First);
-
-                // consider this point only if it's not already in the region.
-                if (outputImage[connectionPoint].GrayLevel != 0) continue;
-
-                // only if growing criteria is met, recurse away.
-                if (criteria(startPoint, connectionPoint, criteriaCookie))
-                {
-                    // recurse into the new connection point.
-                    RegionGrowing(connectionPoint, outputImage, criteria, criteriaCookie);
-                }
-            }
-        }
-
         private void grayscaleTolerance_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == 13)
@@ -177,6 +136,5 @@ namespace OpenCVDotNet.Examples
         }
     }
 
-    delegate bool RegionGrowingCriteria(Point startPt, Point candidatePt, object cookie);
 }
 
