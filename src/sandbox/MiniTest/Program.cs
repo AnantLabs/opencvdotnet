ab05e88c98f179a4cbfea513e05e02c1443befa7
yoavhacohen <yoavhacohen@05b48626-c427-0410-92d5-518f3ac39d56>
2008-01-25 00:34:04 +0000

    ToBitmap was reimplemented. MiniTest was update and now it checks conversion between Bitmap and CVImage.

--- src/sandbox/MiniTest/Program.cs
+++ src/sandbox/MiniTest/Program.cs
@@ -8,13 +8,17 @@ namespace Test
 {
     class Program
     {
+
+        [STAThread]
         static void Main(string[] args)
         {
             CVUtils.ErrorsToExceptions();
-            CVImage image = new CVImage(@"C:\Users\Yoav HaCohen\Pictures\temp\hair_res.bmp");
-            CVHistogram h = image.CalcHistogram(255);
-            MessageBox.Show("Done");
-            MessageBox.Show("!!! " + h.MinMaxValue);
+            //CVImage image = new CVImage(@"C:\Users\Yoav HaCohen\Pictures\temp\hair_res.bmp");
+            CVImage image = new CVImage(new System.Drawing.Bitmap(@"C:\Users\Yoav HaCohen\Pictures\temp\hair_res.bmp"));
+            new BitmapViewer(image.ToBitmap()).ShowDialog();
+            //CVHistogram h = image.CalcHistogram(255);
+            //MessageBox.Show("Done");
+            //MessageBox.Show("!!! " + h.MinMaxValue);
             
         }
     }
